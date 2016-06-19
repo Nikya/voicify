005815ac896e7b3364726f45f8cf11d7176923c9
Yannick Severac <severac.yannick@gmail.com>
2016-06-19 12:50:46 +0200

    Play a prefix sound before tts

--- core/sound_system/OpenKarotz.php
+++ core/sound_system/OpenKarotz.php
@@ -3,6 +3,12 @@
 /** To generate a notification speaked sound */
 class OpenKarotz {
 
+	private $ip = '192.168.1.8';
+
+	private $port = '80';
+
+	private $voice = 'margaux';
+
 	/** Conversion des prefix */
 	const PREFIX_MAP = array (
 			'no' => '',
@@ -10,25 +16,41 @@ class OpenKarotz {
 			'clock' => 'Tic. Tac. '
 		);
 
-	////////////////////////////////////////////////////////////////////////////////
-	/** Send the text to the sound system */
+	/***************************************************************************
+	* Send the text to the sound system
+	*/
 	public function play($tts, $prefix, $cache) {
-		$ip = "192.168.1.8";
-		$port = "80";
-		$voice = "margaux";
-
 		$noCache = $cache ? '0' : '1';
 		$prefixTxt = SELF::PREFIX_MAP[$prefix];
 
 		$enctext = urlencode($prefixTxt . $tts);
 
-		$url = "$ip:$port/cgi-bin/tts?voice=$voice&nocache=$noCache&text=$enctext";
+		$url = "{$this->ip}:{$this->port}/cgi-bin/tts?voice={$this->voice}&nocache=$noCache&text=$enctext";
 
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+		$this->playPrefixSound($prefix);
+
 		$res = curl_exec($ch);
 
 		if ($res===false)
-			throw new Exception("process_tmp fail to process $url");
+			throw new Exception("OpenKarotz fail to play $url");
+	}
+
+	/***************************************************************************
+	* Play a stored sound before playing the tts
+	*/
+	private function playPrefixSound($prefix) {
+		if ($prefix != 'no') {
+			$url = "{$this->ip}:{$this->port}/cgi-bin/sound?id=prefixsound_$prefix";
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			$res = curl_exec($ch);
+
+			if ($res === false)
+				traceError(__FUNCTION__, "Can't play Prefix Sound $prefix with $url");
+		}
 	}
 }
